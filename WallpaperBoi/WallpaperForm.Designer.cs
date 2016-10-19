4441124ef5285c25688438cd548473d94ec43fbd
Arthur Uzulin <fuj1nman@hotmail.com>
2016-10-19 13:09:19 +1100

    Initial commit (mess of fast code)

---
+++ WallpaperBoi/WallpaperForm.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace WallpaperBoi
+{
+    partial class WallpaperForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.wallpaper = new System.Windows.Forms.PictureBox();
+            this.broken = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.wallpaper)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // wallpaper
+            // 
+            this.wallpaper.BackColor = System.Drawing.Color.Transparent;
+            this.wallpaper.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.wallpaper.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.wallpaper.Location = new System.Drawing.Point(0, 0);
+            this.wallpaper.Name = "wallpaper";
+            this.wallpaper.Size = new System.Drawing.Size(799, 592);
+            this.wallpaper.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.wallpaper.TabIndex = 0;
+            this.wallpaper.TabStop = false;
+            // 
+            // broken
+            // 
+            this.broken.BackColor = System.Drawing.Color.Transparent;
+            this.broken.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.broken.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.broken.Location = new System.Drawing.Point(0, 0);
+            this.broken.Name = "broken";
+            this.broken.Size = new System.Drawing.Size(799, 592);
+            this.broken.TabIndex = 1;
+            this.broken.Text = "Gratz Jason, \r\nyou broke it.";
+            this.broken.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.broken.Visible = false;
+            // 
+            // WallpaperForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 592);
+            this.Controls.Add(this.broken);
+            this.Controls.Add(this.wallpaper);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "WallpaperForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "WallpaperForm";
+            this.Load += new System.EventHandler(this.WallpaperForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.wallpaper)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox wallpaper;
+        private System.Windows.Forms.Label broken;
+    }
+}
---
+++ WallpaperBoi/WallpaperForm.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WallpaperBoi
+{
+    public partial class WallpaperForm : Form
+    {
+        public static readonly string PATH = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), "wallpaperoverride.jpg");
+        public static readonly string PATH2 = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), "wallpaperoverride.png");
+
+        public WallpaperForm()
+        {
+            InitializeComponent();
+
+            Width = Screen.PrimaryScreen.Bounds.Width;
+            Height = Screen.PrimaryScreen.Bounds.Height;
+
+            if (!File.Exists(PATH) && !File.Exists(PATH2))
+            {
+                broken.Visible = true;
+                BackColor = Color.White;
+            }
+            else {
+                IntPtr lHwnd = Program.FindWindow("Shell_TrayWnd", null);
+                Program.SendMessage(lHwnd, Program.WM_COMMAND, (IntPtr)Program.MIN_ALL, IntPtr.Zero);
+                System.Threading.Thread.Sleep(1000);
+                wallpaper.BackgroundImage = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+                using (Graphics g = Graphics.FromImage(wallpaper.BackgroundImage))
+                {
+                    g.CopyFromScreen(Screen.PrimaryScreen.Bounds.X,
+                                     Screen.PrimaryScreen.Bounds.Y,
+                                     0, 0,
+                                     wallpaper.BackgroundImage.Size,
+                                     CopyPixelOperation.SourceCopy);
+                }
+                wallpaper.Image = new Bitmap(File.Exists(PATH2) ? PATH2 : PATH);
+                Program.SendMessage(lHwnd, Program.WM_COMMAND, (IntPtr)Program.MIN_ALL_UNDO, IntPtr.Zero);
+            }
+        }
+
+        private void WallpaperForm_Load(object sender, EventArgs e)
+        {
+            IntPtr progman = Program.FindWindow("Progman", null);
+            IntPtr result = IntPtr.Zero;
+            Program.SendMessageTimeout(progman, 0x052C, new IntPtr(0), IntPtr.Zero, Program.SendMessageTimeoutFlags.SMTO_NORMAL, 1000, out result);
+            IntPtr workerW = IntPtr.Zero;
+            Program.EnumWindows(new Program.EnumWindowsProc((tophandle, topparamhandle) =>
+            {
+                IntPtr p = Program.FindWindowEx(tophandle, IntPtr.Zero, "SHELLDLL_DefView", null);
+                if(p != IntPtr.Zero)
+                {
+                    workerW = Program.FindWindowEx(IntPtr.Zero, tophandle, "WorkerW", null);
+                }
+
+                return true;
+            }
+            ), IntPtr.Zero);
+
+            Program.ShowWindow(workerW, Program.ShowWindowCommands.Hide);
+            Program.SetParent(Handle, progman);
+        }
+    }
+}
