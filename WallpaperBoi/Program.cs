d98750f1076bb5b2c6d414793c15d4769582855e
Arthur Uzulin <fuj1nman@hotmail.com>
2016-10-19 17:11:34 +1100

    Cleanup, use Shell API to show desktop, additional formats supported (including gif), watch filesystem for changes, no longer lock the image files.

--- WallpaperBoi/Program.cs
+++ WallpaperBoi/Program.cs
@@ -1,19 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Drawing;
 
 namespace WallpaperBoi
 {
     static class Program
     {
-        public const int WM_COMMAND = 0x111;
-        public const int MIN_ALL = 419;
-        public const int MIN_ALL_UNDO = 416;
-
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -22,155 +13,6 @@ namespace WallpaperBoi
         {
             Application.EnableVisualStyles();
             Application.Run(new WallpaperForm());
-        }
-
-        [DllImport("user32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        public static extern bool ShowWindow(IntPtr hWnd, ShowWindowCommands nCmdShow);
-        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-        public static extern IntPtr SendMessageTimeout(IntPtr windowHandle, uint Msg, IntPtr wParam, IntPtr lParam, SendMessageTimeoutFlags flags, uint timeout, out IntPtr result);
-        [DllImport("user32.dll", SetLastError = true)]
-        public static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
-        [DllImport("user32.dll")]
-        public static extern IntPtr GetDCEx(IntPtr hWnd, IntPtr hrgnClip, DeviceContextValues flags);
-        [DllImport("user32.dll", SetLastError = true)]
-        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
-        [DllImport("user32.dll", SetLastError = true)]
-        public static extern IntPtr FindWindowEx(IntPtr parentHandle, IntPtr childAfter, string className, string windowTitle);
-
-        [DllImport("user32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        public static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);
-        public delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
-
-        [DllImport("user32.dll", EntryPoint = "SendMessage", SetLastError = true)]
-        public static extern IntPtr SendMessage(IntPtr hWnd, Int32 Msg, IntPtr wParam, IntPtr lParam);
-
-        [Flags]
-        public enum SendMessageTimeoutFlags : uint
-        {
-            SMTO_NORMAL = 0x0,
-            SMTO_BLOCK = 0x1,
-            SMTO_ABORTIFHUNG = 0x2,
-            SMTO_NOTIMEOUTIFNOTHUNG = 0x8,
-            SMTO_ERRORONEXIT = 0x20
-        }
-
-        /// <summary>Values to pass to the GetDCEx method.</summary>
-        [Flags()]
-        public enum DeviceContextValues : uint
-        {
-            /// <summary>DCX_WINDOW: Returns a DC that corresponds to the window rectangle rather 
-            /// than the client rectangle.</summary>
-            Window = 0x00000001,
-            /// <summary>DCX_CACHE: Returns a DC from the cache, rather than the OWNDC or CLASSDC 
-            /// window. Essentially overrides CS_OWNDC and CS_CLASSDC.</summary>
-            Cache = 0x00000002,
-            /// <summary>DCX_NORESETATTRS: Does not reset the attributes of this DC to the 
-            /// default attributes when this DC is released.</summary>
-            NoResetAttrs = 0x00000004,
-            /// <summary>DCX_CLIPCHILDREN: Excludes the visible regions of all child windows 
-            /// below the window identified by hWnd.</summary>
-            ClipChildren = 0x00000008,
-            /// <summary>DCX_CLIPSIBLINGS: Excludes the visible regions of all sibling windows 
-            /// above the window identified by hWnd.</summary>
-            ClipSiblings = 0x00000010,
-            /// <summary>DCX_PARENTCLIP: Uses the visible region of the parent window. The 
-            /// parent's WS_CLIPCHILDREN and CS_PARENTDC style bits are ignored. The origin is 
-            /// set to the upper-left corner of the window identified by hWnd.</summary>
-            ParentClip = 0x00000020,
-            /// <summary>DCX_EXCLUDERGN: The clipping region identified by hrgnClip is excluded 
-            /// from the visible region of the returned DC.</summary>
-            ExcludeRgn = 0x00000040,
-            /// <summary>DCX_INTERSECTRGN: The clipping region identified by hrgnClip is 
-            /// intersected with the visible region of the returned DC.</summary>
-            IntersectRgn = 0x00000080,
-            /// <summary>DCX_EXCLUDEUPDATE: Unknown...Undocumented</summary>
-            ExcludeUpdate = 0x00000100,
-            /// <summary>DCX_INTERSECTUPDATE: Unknown...Undocumented</summary>
-            IntersectUpdate = 0x00000200,
-            /// <summary>DCX_LOCKWINDOWUPDATE: Allows drawing even if there is a LockWindowUpdate 
-            /// call in effect that would otherwise exclude this window. Used for drawing during 
-            /// tracking.</summary>
-            LockWindowUpdate = 0x00000400,
-            /// <summary>DCX_USESTYLE: Undocumented, something related to WM_NCPAINT message.</summary>
-            UseStyle = 0x00010000,
-            /// <summary>DCX_VALIDATE When specified with DCX_INTERSECTUPDATE, causes the DC to 
-            /// be completely validated. Using this function with both DCX_INTERSECTUPDATE and 
-            /// DCX_VALIDATE is identical to using the BeginPaint function.</summary>
-            Validate = 0x00200000,
-        }
-
-        public enum ShowWindowCommands
-        {
-            /// <summary>
-            /// Hides the window and activates another window.
-            /// </summary>
-            Hide = 0,
-            /// <summary>
-            /// Activates and displays a window. If the window is minimized or 
-            /// maximized, the system restores it to its original size and position.
-            /// An application should specify this flag when displaying the window 
-            /// for the first time.
-            /// </summary>
-            Normal = 1,
-            /// <summary>
-            /// Activates the window and displays it as a minimized window.
-            /// </summary>
-            ShowMinimized = 2,
-            /// <summary>
-            /// Maximizes the specified window.
-            /// </summary>
-            Maximize = 3, // is this the right value?
-                          /// <summary>
-                          /// Activates the window and displays it as a maximized window.
-                          /// </summary>       
-            ShowMaximized = 3,
-            /// <summary>
-            /// Displays a window in its most recent size and position. This value 
-            /// is similar to <see cref="Win32.ShowWindowCommand.Normal"/>, except 
-            /// the window is not activated.
-            /// </summary>
-            ShowNoActivate = 4,
-            /// <summary>
-            /// Activates the window and displays it in its current size and position. 
-            /// </summary>
-            Show = 5,
-            /// <summary>
-            /// Minimizes the specified window and activates the next top-level 
-            /// window in the Z order.
-            /// </summary>
-            Minimize = 6,
-            /// <summary>
-            /// Displays the window as a minimized window. This value is similar to
-            /// <see cref="Win32.ShowWindowCommand.ShowMinimized"/>, except the 
-            /// window is not activated.
-            /// </summary>
-            ShowMinNoActive = 7,
-            /// <summary>
-            /// Displays the window in its current size and position. This value is 
-            /// similar to <see cref="Win32.ShowWindowCommand.Show"/>, except the 
-            /// window is not activated.
-            /// </summary>
-            ShowNA = 8,
-            /// <summary>
-            /// Activates and displays the window. If the window is minimized or 
-            /// maximized, the system restores it to its original size and position. 
-            /// An application should specify this flag when restoring a minimized window.
-            /// </summary>
-            Restore = 9,
-            /// <summary>
-            /// Sets the show state based on the SW_* value specified in the 
-            /// STARTUPINFO structure passed to the CreateProcess function by the 
-            /// program that started the application.
-            /// </summary>
-            ShowDefault = 10,
-            /// <summary>
-            ///  <b>Windows 2000/XP:</b> Minimizes a window, even if the thread 
-            /// that owns the window is not responding. This flag should only be 
-            /// used when minimizing windows from a different thread.
-            /// </summary>
-            ForceMinimize = 11
         }
     }
 }
--- WallpaperBoi/WallpaperForm.cs
+++ WallpaperBoi/WallpaperForm.cs
@@ -1,71 +1,170 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using WallpaperBoi.natives;
 
 namespace WallpaperBoi
 {
+    /**
+     * The form that is attached to Progman
+     */
     public partial class WallpaperForm : Form
     {
-        public static readonly string PATH = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), "wallpaperoverride.jpg");
-        public static readonly string PATH2 = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), "wallpaperoverride.png");
+        // The search path
+        public static readonly string PATH = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+
+        // The file name
+        public const string FILENAME = "wallpaperoverride";
+
+        // The extensions the file can be in
+        public static readonly string[] SUPPORTED_EXTENSIONS = { "png", "jpg", "bmp", "gif", "tif", "tiff" };
+
+        private Shell32.ShellClass shell;
+
+        // File watcher
+        private FileSystemWatcher watcher;
 
         public WallpaperForm()
         {
             InitializeComponent();
 
+            // Create shell access
+            shell = new Shell32.ShellClass();
+
+            // Resize to screen size
             Width = Screen.PrimaryScreen.Bounds.Width;
             Height = Screen.PrimaryScreen.Bounds.Height;
 
-            if (!File.Exists(PATH) && !File.Exists(PATH2))
+            LoadDesktop();
+            LoadImage();
+
+            // Create a file system watcher to watch for changes to the pictures
+            watcher = new FileSystemWatcher(PATH);
+            watcher.Changed += FileChanged;
+            watcher.Created += FileChanged;
+            watcher.Deleted += FileChanged;
+            watcher.Renamed += FileChanged;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        public void FileChanged(object sender, FileSystemEventArgs args)
+        {
+            // If we're on a different thread, invoke FileChanged_Local on main thread, otherwise simply call FileChanged_Local
+            if (InvokeRequired)
             {
-                broken.Visible = true;
-                BackColor = Color.White;
+                MethodInvoker method = new MethodInvoker(FileChanged_Local);
+                Invoke(method);
             }
-            else {
-                IntPtr lHwnd = Program.FindWindow("Shell_TrayWnd", null);
-                Program.SendMessage(lHwnd, Program.WM_COMMAND, (IntPtr)Program.MIN_ALL, IntPtr.Zero);
-                System.Threading.Thread.Sleep(1000);
-                wallpaper.BackgroundImage = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
-                using (Graphics g = Graphics.FromImage(wallpaper.BackgroundImage))
-                {
-                    g.CopyFromScreen(Screen.PrimaryScreen.Bounds.X,
-                                     Screen.PrimaryScreen.Bounds.Y,
-                                     0, 0,
-                                     wallpaper.BackgroundImage.Size,
-                                     CopyPixelOperation.SourceCopy);
-                }
-                wallpaper.Image = new Bitmap(File.Exists(PATH2) ? PATH2 : PATH);
-                Program.SendMessage(lHwnd, Program.WM_COMMAND, (IntPtr)Program.MIN_ALL_UNDO, IntPtr.Zero);
+            else
+            {
+                FileChanged_Local();
             }
         }
 
-        private void WallpaperForm_Load(object sender, EventArgs e)
+        public void FileChanged_Local()
         {
-            IntPtr progman = Program.FindWindow("Progman", null);
-            IntPtr result = IntPtr.Zero;
-            Program.SendMessageTimeout(progman, 0x052C, new IntPtr(0), IntPtr.Zero, Program.SendMessageTimeoutFlags.SMTO_NORMAL, 1000, out result);
-            IntPtr workerW = IntPtr.Zero;
-            Program.EnumWindows(new Program.EnumWindowsProc((tophandle, topparamhandle) =>
+            LoadImage();
+        }
+
+        // Iterates through the extensions and tries to find the file, returns the first found file
+        public string FindFile()
+        {
+            foreach (string ex in SUPPORTED_EXTENSIONS)
             {
-                IntPtr p = Program.FindWindowEx(tophandle, IntPtr.Zero, "SHELLDLL_DefView", null);
-                if(p != IntPtr.Zero)
+                string file = Path.Combine(PATH, FILENAME + "." + ex);
+                if (File.Exists(file))
+                    return file;
+            }
+
+            return "MISSINGNO;/";
+        }
+
+        // Loads the override image or displays the broken text
+        public void LoadImage()
+        {
+            broken.Visible = false;
+
+            if (wallpaper.Image != null)
+            {
+                wallpaper.Image.Dispose();
+                wallpaper.Image = null;
+            }
+
+            string file = FindFile();
+
+            if (file.Equals("MISSINGNO;/"))
+            {
+                broken.Visible = true;
+            }
+            else
+            {
+                // Reads the image without locking the file
+                using (FileStream stream = new FileStream(file, FileMode.Open, FileAccess.Read))
                 {
-                    workerW = Program.FindWindowEx(IntPtr.Zero, tophandle, "WorkerW", null);
+                    wallpaper.Image = Image.FromStream(stream);
+                }
+            }
+        }
+
+        // Takes a screenshot of the desktop and makes that our background
+        private void LoadDesktop()
+        {
+            ToggleDesktop();
+            ToggleIcons();
+            System.Threading.Thread.Sleep(1000);
+            wallpaper.BackgroundImage = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            using (Graphics g = Graphics.FromImage(wallpaper.BackgroundImage))
+            {
+                g.CopyFromScreen(Screen.PrimaryScreen.Bounds.X,
+                                 Screen.PrimaryScreen.Bounds.Y,
+                                 0, 0,
+                                 wallpaper.BackgroundImage.Size,
+                                 CopyPixelOperation.SourceCopy);
+            }
+            ToggleIcons();
+            ToggleDesktop();
+        }
+
+        // Acts like the show desktop button
+        private void ToggleDesktop()
+        {
+            ((Shell32.IShellDispatch4)shell).ToggleDesktop();
+        }
+
+        // Toggles desktop icons
+        private void ToggleIcons()
+        {
+            IntPtr hWnd = User32.GetWindow(User32.FindWindow("Progman", "Program Manager"), User32.GetWindowType.GW_CHILD);
+            User32.SendMessage(hWnd, User32.WM_COMMAND, new IntPtr(User32.TOGGLE_DESKTOP), IntPtr.Zero);
+        }
+
+        // Attaches the form to Program Manager
+        private void AttachToProgman()
+        {
+            IntPtr progman = User32.FindWindow("Progman", null);
+            IntPtr result = IntPtr.Zero;
+            User32.SendMessageTimeout(progman, 0x052C, new IntPtr(0), IntPtr.Zero, User32.SendMessageTimeoutFlags.SMTO_NORMAL, 1000, out result);
+            IntPtr workerW = IntPtr.Zero;
+            User32.EnumWindows(new User32.EnumWindowsProc((tophandle, topparamhandle) =>
+            {
+                IntPtr p = User32.FindWindowEx(tophandle, IntPtr.Zero, "SHELLDLL_DefView", null);
+                if (p != IntPtr.Zero)
+                {
+                    workerW = User32.FindWindowEx(IntPtr.Zero, tophandle, "WorkerW", null);
                 }
 
                 return true;
             }
             ), IntPtr.Zero);
 
-            Program.ShowWindow(workerW, Program.ShowWindowCommands.Hide);
-            Program.SetParent(Handle, progman);
+            User32.ShowWindow(workerW, User32.ShowWindowCommands.Hide);
+            User32.SetParent(Handle, progman);
+        }
+
+        private void WallpaperForm_Load(object sender, EventArgs e)
+        {
+            AttachToProgman();
         }
     }
 }
